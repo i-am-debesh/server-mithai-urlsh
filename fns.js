ef9c475b7ce2549f12ca55e2a1465794802a1bf5
Debesh <debesh391@gmail.com>
2025-05-21 11:45:59 +0530

    first commit

---
+++ fns.js
@@ -0,0 +1,45 @@
+import { Url } from "./model.js";
+
+function shortURL(generatedID) {
+
+    const customURL = `https://mithai-0t52.onrender.com/id?url=${generatedID}`;
+
+    const localURL = `http://localhost:5000/id?url=${generatedID}`;
+
+    return customURL;
+}
+
+async function getUniqueID() {
+
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let isUnique = false;
+    let code = '';
+
+    while (!isUnique) {
+        // Generate 3-character code : can generate upto 62^3 = 238,328 combinations
+        code = '';
+        for (let i = 0; i < 3; i++) {
+            code += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        // Check if it already exists in the database
+        const existing = await Url.findOne({ shortCode: code });
+        if (!existing) {
+            isUnique = true;
+        }
+    }
+
+    return code;
+}
+
+
+
+async function getFullLink(id){
+    const record = await Url.findOne({shortCode: id});
+    if(record) {
+        return record.fullUrl;
+    }else {
+        return -1;
+    }
+}
+export {shortURL, getUniqueID, getFullLink}
---
+++ model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+const UrlSchema = new mongoose.Schema({
+    shortCode: String,
+    fullUrl: String,
+    shortUrl: String
+});
+const Url = mongoose.model('Url', UrlSchema);
+
+async function saveURL(shortCode,fullUrl,shortUrl) {
+    const newUrl = new Url(
+        { 
+            shortCode: shortCode, 
+            fullUrl: fullUrl,
+            shortUrl: shortUrl
+        
+        });
+    newUrl.save().then(() => console.log("Saved to DB"));
+}
+
+export {saveURL, Url}
